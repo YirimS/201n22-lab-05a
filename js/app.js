babc1bf4d5cc7d8c2c57f0061fe62b652219b20d
YirimS <yirimseck@gmail.com>
2021-05-26 19:08:47 -0700

    fixed syntax

--- js/app.js
+++ js/app.js
@@ -98,12 +98,12 @@ function sumArray(sumArr) { //eslint-disable-line
   let message4 = `${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${sumTotal} is their sum.`;
   //console.log(sumTotal, message4);
   return [sumTotal, message4];
-  
+
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
- testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -119,19 +119,19 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-  let product = multiply(sumArr[0], sumArr[1])[0];
-    product = multiply(product, sumArr[2])[0];
-    //console.log(sumTotal);
-    let message4 = `${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${sumTotal} is their sum.`;
-    //console.log(sumTotal, message4);
-    return [sumTotal, message4];
-    
-  }
+  let product = multiply(multArr[0], multArr[1])[0];
+  product = multiply(product, multArr[2])[0];
+  //console.log(sumTotal);
+  let message4 = `The numbers ${multArr[0]},${multArr[1]},${multArr[2]} have a product of ${product}.`;
+  //console.log(sumTotal, message4);
+  return [product, message4];
+
+
 }
-    //
+//
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
